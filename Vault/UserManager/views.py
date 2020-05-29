f582fd788bcf832bc9f0269fd169973d11d11538
NielsVanM <niels.van.marion@quicknet.nl>
2020-05-29 14:02:31 +0200

    Finished login backend, added .vscode/ to gitignore

--- Vault/UserManager/views.py
+++ Vault/UserManager/views.py
@@ -1,10 +1,35 @@
 from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User, Group
+import logging
+
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
+
+from django.shortcuts import HttpResponse
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 class LoginView(TemplateView):
     template_name = "user/login.html"
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        if username == None or password == None:
+            logger.debug("Authentication Attempt with empty username/password")
+            return HttpResponse("Wun is empty")
+        
+        logger.debug(f"Authentication attempt for {username}")
+
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            return redirect("/users/login/")
+        
+        login(request, user)
+        return redirect("/files/")
 
 class RegisterView(TemplateView):
     template_name = "user/register.html"
