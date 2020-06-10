f6d1793e186455c8ab9c6d01f687dd477a6778de
Roy van Bragt <roy@van-bragt.com>
2020-06-10 13:34:22 +0200

    Created access management file & start GrantAccess.

---
+++ Vault/FileManager/access_management.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+
+from FileManager.models import AccessRights
+
+def GrantAccess(User):
+    """Provides designated permissions to user.
+    Object ID can be either a file or folder."""
+
+    GA = AccessRights(target_user=User,
+                      access_right=ACCESS_RIGHT_CHOICES,
+                      object_id=object_id,
+                      content_object=content_object,
+                      content_type=content_type)
+
+    GA.save()
+
