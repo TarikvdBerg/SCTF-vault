2642ebd0a0a490f6a60a4253bb740d455ba0efd2
Roy van Bragt <roy@van-bragt.com>
2020-06-11 17:18:45 +0200

    Finalized GrantAccess func (check content_object)

--- Vault/FileManager/access_management.py
+++ Vault/FileManager/access_management.py
@@ -1,17 +1,22 @@
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
 
 from FileManager.models import AccessRights
 
 def GrantAccess(target_user, access_right, object_id,
-                content_object, content_type):
+                # content_object, 
+                content_type):
     """Provides designated permissions to user.
     Object ID can be either a file or folder."""
 
-    GA = AccessRights(target_user=User,
-                      access_right=ACCESS_RIGHT_CHOICES,
-                      object_id=object_id,
-                      content_object=content_object,
-                      content_type=content_type)
+    roy = User.objects.get(username="roy")
+    CT = ContentType.objects.get(model="accessright")
+
+    GA = AccessRights(target_user=roy,
+                      access_right="RO",
+                      object_id="2fb43a0f-b4ce-4fff-bf12-1aa9bd308cec",
+                      # content_object=content_object,
+                      content_type=CT)
 
     GA.save()
     print("Granted access.")
--- Vault/FileManager/management/commands/AM.py
+++ Vault/FileManager/management/commands/AM.py
@@ -1,4 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
 
 from FileManager.models import AccessRights
 from FileManager.access_management import GrantAccess
@@ -7,6 +9,10 @@ class Command(BaseCommand):
 
     def handle(self, **options):
 
-        GrantAccess(target_user=boogeyman, access_right=RO,
+        roy = User.objects.get(username="roy")
+        CT = ContentType.objects.get(model="accessright")
+        
+        GrantAccess(target_user=roy, access_right="RO",
         object_id="2fb43a0f-b4ce-4fff-bf12-1aa9bd308cec",
-        content_object=content_object, content_type=content_type)
+        # content_object=content_object, 
+        content_type=CT)
